0c42725de4da95010bf8da66a816eba93c771210
MalickMuzamil <malikmuzamil92110@gmail.com>
2025-12-13 18:41:29 +0500

    UI updates and dropdown improvements

--- src/app/Layouts/main-layout/main-layout.ts
+++ src/app/Layouts/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from "../../Shared/navbar/navbar";
 import {Footer} from "../../Shared/footer/footer";
+import { Whatsapp } from '../../Shared/whatsapp/whatsapp';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Whatsapp],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
