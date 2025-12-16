e21bb5f1733b9c0d908f36f871ef6fec8f583042
MalickMuzamil <malikmuzamil92110@gmail.com>
2025-12-16 17:59:57 +0500

    Added some minor updates + Forms Functional

--- src/app/Components/Contact/contact-form/contact-form.ts
+++ src/app/Components/Contact/contact-form/contact-form.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { Modal } from '../../../Shared/modal/modal';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  standalone: true,
+  imports: [Modal, FormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.css',
 })
 export class ContactForm {
+  modalVisible = false;
+  modalTitle = '';
+  modalMessage = '';
+  isSending = false;
 
+  constructor() {
+    emailjs.init('IhUnYU45kPjCq5Hpr');
+  }
+
+  sendEmail(e: Event) {
+    this.isSending = true;
+
+    emailjs
+      .sendForm(
+        'service_hzmjwoq',
+        'template_vt295ku',
+        e.target as HTMLFormElement
+      )
+      .then(
+        () => {
+          this.modalTitle = 'Message Sent ✅';
+          this.modalMessage =
+            'Thank you for contacting us. We will get back to you shortly.';
+          this.modalVisible = true;
+          this.isSending = false;
+          (e.target as HTMLFormElement).reset();
+        },
+        () => {
+          this.modalTitle = 'Error ❌';
+          this.modalMessage = 'Something went wrong. Please try again later.';
+          this.modalVisible = true;
+          this.isSending = false;
+        }
+      );
+  }
+
+  onModalClosed() {
+    this.modalVisible = false;
+  }
 }
--- src/app/Components/Contact/contact-hero/contact-hero.ts
+++ src/app/Components/Contact/contact-hero/contact-hero.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './contact-hero.css',
 })
 export class ContactHero {
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const yOffset = -20;
+      const y =
+        element.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
